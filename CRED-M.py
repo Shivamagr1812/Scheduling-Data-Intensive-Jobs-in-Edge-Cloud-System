cce9c3c199a775116a744dcd79583c8d66b5026a
Himanshi <ghimanshi941@gmail.com>
2024-04-15 18:47:26 +0530

    added finished attr

--- CRED-M.py
+++ CRED-M.py
@@ -1,3 +1,26 @@
+def schedule(C, NTS_rd):
+    # Sort C based on the remaining number of required time slots for d in ascending order
+    C.sort(key=lambda x: x[1])  # Assuming each chunk in C is represented as (chunk_id, remaining_time_slots, finished)
+
+    for chunk_index in range(len(C)):
+        chunk_id, remaining_time_slots, finished = C[chunk_index]
+
+        # Check if the chunk has not been finished yet
+        if not finished:
+            # Check if remaining time slots of the chunk is greater than 0
+            if remaining_time_slots - NTS_rd > 0:
+                # Update remaining time slots and NTS_rd
+                remaining_time_slots -= NTS_rd
+                NTS_rd = 0
+                C[chunk_index] = (chunk_id, remaining_time_slots, finished)  # Update the remaining time slots in the original list
+                break
+            else:
+                # Update NTS_rd and set remaining time slots to 0
+                NTS_rd -= remaining_time_slots
+                C[chunk_index] = (chunk_id, 0, True)  # Set remaining time slots to 0 and mark chunk as finished
+
+    return C, NTS_rd
+
 def CRED_M(nodes, chunks, deadlines, B, jobs, S): 
     D = len(deadlines)
     num_active_nodes = 0
@@ -66,6 +89,7 @@ class Chunk:
     def __init__(self, id, slots_required):
         self.id = id
         self.slots_required = slots_required
+        self.finished = False
 
 chunks = []
 for i in range(len(jobs)):
