315c6ab5a411990339d5b2325166812f5ed6970f
Shivam Agrawal <shivam.agrawal@iitg.ac.in>
2024-04-15 14:16:51 +0530

    Added CRED-M

---
+++ CRED-M.py
@@ -0,0 +1,93 @@
+def CRED_M(nodes, chunks, deadlines, B, jobs, S): 
+    D = len(deadlines)
+    num_active_nodes = 0
+    
+    for i in range(D):
+        num_active_i = CRED_S(deadlines[i], chunks, B, jobs[i].deadline)
+        for n in range(num_active_i):
+            for j in range(i+1, D):
+                schedule_chunks(nodes[n].chunks_scheduled, S*d[j] - count_scheduled_slots(n))
+        num_active_nodes += num_active_i
+    
+    return num_active_nodes
+
+def CRED_S(Ci, F, B, d_i):
+    # Implementation of CRED-S function
+    pass
+
+def schedule_chunks(C, NTS):
+    # Implementation of chunk scheduling
+    pass
+
+def count_scheduled_slots(node):
+    # Implementation of counting scheduled slots
+    pass
+
+
+# User Input
+J = int(input("Enter the number of jobs: "))
+N = int(input("Enter the number of nodes: "))
+B = int(input("Enter the maximum number of data chunks each node can host: "))
+S = int(input("Enter the number of VMs (virtual machines) on each node: "))
+
+class Job:
+    def __init__(self, deadline, chunks_required):
+        self.deadline = deadline
+        self.chunks_required = chunks_required
+
+jobs = []
+for j in range(J):
+    deadline = int(input(f"Enter the deadline for job {j+1}: "))
+    chunks_required = input(f"Enter the list of chunk ids required for job {j+1} (comma-separated): ").split(',')
+    chunks_required = [int(chunk_id) for chunk_id in chunks_required]
+    job = Job(deadline, chunks_required)
+    jobs.append(job)
+
+class Chunk:
+    def __init__(self, id, slots_required):
+        self.id = id
+        self.slots_required = slots_required
+
+chunks = []
+for i in range(len(jobs)):
+    for chunk_id in jobs[i].chunks_required:
+        chunk_exists = False
+        for chunk in chunks:
+            if chunk.id == chunk_id:
+                chunk_exists = True
+                break
+        
+        if chunk_exists:
+            chunk.slots_required[jobs[i].deadline] += 1
+
+        slots_required = []
+        slots_required[jobs[i].deadline] = 1
+        chunk = Chunk(chunk_id, slots_required)
+        chunks.append(chunk)
+
+class Deadline:
+    def __init__(self, deadline, chunks_required):
+        self.deadline = deadline
+        self.chunks_required = chunks_required
+
+deadlines = []
+for job in jobs:
+    chunks_required = [chunk_id for chunk_id in job.chunks_required]
+    
+    if job.deadline not in deadlines:
+        deadline = Deadline(job.deadline, chunks_required)
+        deadlines.append(deadline)
+    else:
+        for deadline in deadlines:
+            if deadline.deadline == job.deadline:
+                deadline.chunks_required.append(chunks_required)    
+
+class Node:
+    def __init__(self, chunks_scheduled):
+        self.chunks_scheduled = chunks_scheduled
+
+nodes = []
+for n in range(N):
+    chunks_scheduled = []
+    node = Node(chunks_scheduled)
+    nodes.append(node)
